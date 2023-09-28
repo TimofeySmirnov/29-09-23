76bcec3f078ab0553b805c84d79656db2c45fd25
TimofeySmirnov <115399337+TimofeySmirnov@users.noreply.github.com>
2023-09-29 00:20:39 +0300

    Add files via upload

---
+++ 6/6/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число от 1 до 5:");
+double eee = Convert.ToDouble(Console.ReadLine());
+switch (eee)
+{
+    case 1:
+        Console.WriteLine("Плохо");
+        break;
+    case 2:
+        Console.WriteLine("Неудовлетворительно");
+        break;
+    case 3:
+        Console.WriteLine("Удовлетворительно");
+        break;
+    case 4:
+        Console.WriteLine("Хорошо");
+        break;
+    case 5:
+        Console.WriteLine("Отлично");
+        break;
+    default:
+        Console.WriteLine("Введите число от 1 до 5!!!");
+        break;
+
+}
